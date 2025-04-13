0d50177e9d9e37b26657e1e57e819b1a3b33216b
CecileVexe <cecile.valte@gmail.com>
2025-04-13 15:40:07 +0200

    hotfix: fix migrations

--- prisma/migrations/20250411211214_add_comment_ressource_relations/migration.sql
+++ prisma/migrations/20250411211214_add_comment_ressource_relations/migration.sql
@@ -9,33 +9,5 @@
 -- AlterTable
 ALTER TABLE `comment` ADD COLUMN `ressourceId` VARCHAR(191) NOT NULL;
 
--- AlterTable
-ALTER TABLE `ressource` DROP COLUMN `banner`,
-    DROP COLUMN `file`,
-    ADD COLUMN `bannerId` VARCHAR(191) NULL,
-    ADD COLUMN `fileId` VARCHAR(191) NULL;
-
--- CreateTable
-CREATE TABLE `File` (
-    `id` VARCHAR(191) NOT NULL,
-    `path` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `Image` (
-    `id` VARCHAR(191) NOT NULL,
-    `url` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- AddForeignKey
-ALTER TABLE `Ressource` ADD CONSTRAINT `Ressource_fileId_fkey` FOREIGN KEY (`fileId`) REFERENCES `File`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE `Ressource` ADD CONSTRAINT `Ressource_bannerId_fkey` FOREIGN KEY (`bannerId`) REFERENCES `Image`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
 -- AddForeignKey
 ALTER TABLE `Comment` ADD CONSTRAINT `Comment_ressourceId_fkey` FOREIGN KEY (`ressourceId`) REFERENCES `Ressource`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;

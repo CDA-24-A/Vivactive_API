4f0f0fe9869f631840c707a7cb277568ac487688
CecileVexe <cecile.valte@gmail.com>
2025-04-23 11:53:59 +0200

    hotfix: comment citizen id

--- src/Ressource/Ressource.service.ts
+++ src/Ressource/Ressource.service.ts
@@ -235,6 +235,7 @@ export class RessourceService {
               updatedAt: true,
               citizen: {
                 select: {
+                  id: true,
                   name: true,
                   surname: true,
                 },

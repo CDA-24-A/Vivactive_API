7d0ff1861a2357fece99fe0a51bd1ee21c800db0
Benjamin <langlois.ben10@free.fr>
2025-04-24 15:17:38 +0200

    hotfix : patch category

--- src/category/dto/update-category.dto.ts
+++ src/category/dto/update-category.dto.ts
@@ -3,10 +3,8 @@ import { CreateCategoryDto } from './create-category.dto';
 import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
-    @IsNotEmpty()
     @IsOptional()
     name: string;
-    @IsNotEmpty()
     @IsOptional()
     description: string;
 }

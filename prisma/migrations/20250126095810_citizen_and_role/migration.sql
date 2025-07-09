35a53ccaea1ad95e9e8925baaa395f44baf8f4bf
sogluk76 <lukasgosselin@orange.fr>
2025-07-09 14:39:19 +0200

    fix: test

--- prisma/migrations/20250126095810_citizen_and_role/migration.sql
+++ prisma/migrations/20250126095810_citizen_and_role/migration.sql
@@ -4,8 +4,6 @@
   - You are about to drop the `test` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropTable
-DROP TABLE `Test`;
 
 -- CreateTable
 CREATE TABLE `Citizen` (

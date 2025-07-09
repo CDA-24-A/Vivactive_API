8aaf0e87a8d7c63585f8853486344769cb73630a
Benjamin <langlois.ben10@free.fr>
2025-07-09 17:12:29 +0200

    fix: suppressions des cors

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:8081'],
-  });
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
